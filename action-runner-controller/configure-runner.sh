fe0c5af5b3f5fa061808d4fd987b727cb59127dc
r1anl3 <levu1409@gmail.com>
2024-08-21 21:09:20 +0700

    add arc

---
+++ action-runner-controller/configure-runner.sh
@@ -0,0 +1,10 @@
+INSTALLATION_NAME="arc-runner-set"
+NAMESPACE="arc-runners"
+GITHUB_CONFIG_URL="https://github.com/<YOUR_REPO>"
+GITHUB_PAT="<PAT>"
+helm install "${INSTALLATION_NAME}" \
+    --namespace "${NAMESPACE}" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_PAT}" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
