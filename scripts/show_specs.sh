f8b0d65375e72920eed1105b30b5095c1685e18a
r1anl3 <levu1409@gmail.com>
2024-12-05 15:55:37 +0700

    something here

---
+++ scripts/show_specs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Show kernel version and system architecture"
+uname -a 
+
+echo "Show name and version of distribution"
+head -n1 /etc/issue
+
+echo "Show all partitions registered on the system"
+cat /proc/partitions
+
+echo "Show RAM total seen by the system"
+grep MemTotal /proc/meminfo
+
+echo "Show CPU(s) info"
+grep "model name" /proc/cpuinfo
+
+echo "Show info about disk sda"
+hdparm -i /dev/sda
+
+exit 1
